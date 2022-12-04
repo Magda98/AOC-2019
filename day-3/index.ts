7adaa596570b109bff3e7a1c1d9c57cd266489d1
Magda98 <magdakochman7@gmail.com>
2022-12-04 13:40:45 +0100

    day 3

--- day-3/index.ts
+++ day-3/index.ts
@@ -1,14 +1,15 @@
 /* eslint-disable no-console */
 import fs from 'fs';
-import { chunk, countBy, intersection, upperCase } from 'lodash-es';
-
+import {
+  chunk, intersection, upperCase,
+} from 'lodash-es';
 
 const filepath = new URL('day3.txt', import.meta.url);
 const data = await fs.promises.readFile(filepath, 'utf8');
 
 const input = data.split('\n');
 
-function split(str, index) {
+function split(str: string, index: number) {
   const result = [str.slice(0, index), str.slice(index)];
 
   return result;
@@ -17,9 +18,9 @@ function split(str, index) {
 function getCharaterValue(character: string): number {
   const asciiCode = character.charCodeAt(0);
 
-  if (character === upperCase(character)) return (asciiCode - 38)
+  if (character === upperCase(character)) return (asciiCode - 38);
 
-  return (asciiCode - 96)
+  return (asciiCode - 96);
 }
 
 let score = 0;
@@ -30,8 +31,7 @@ input.forEach((line) => {
 
   const [character] = intersection(firstArray, secondArray);
   score += getCharaterValue(character);
-
-})
+});
 
 // part one answer
 console.log(score);
@@ -40,15 +40,14 @@ const chunkedInput = chunk(input, 3);
 
 let scorePartTwo = 0;
 chunkedInput.forEach((lines) => {
-  const [first, second, third] = lines
+  const [first, second, third] = lines;
   const firstArray = [...first];
   const secondArray = [...second];
   const thirdArray = [...third];
 
   const [character] = intersection(firstArray, secondArray, thirdArray);
   scorePartTwo += getCharaterValue(character);
-
-})
+});
 
 // part two answer
 console.log(scorePartTwo);
