afab1af9312d7d6c67e6209ad0872aef45a4e870
Magda98 <magdakochman7@gmail.com>
2022-12-07 20:42:40 +0100

    day 7

--- day-7/index.ts
+++ day-7/index.ts
@@ -6,8 +6,6 @@ const filepath = new URL('day7.txt', import.meta.url);
 const data = await fs.promises.readFile(filepath, 'utf8');
 const input = data.split('\n');
 
-console.log(input);
-
 const path: string[] = [];
 let directorySum: Record<string, number> = {};
 
@@ -36,8 +34,9 @@ const sum = arrayOfValues.reduce((acc, cur) => (cur <= 100_000 ? acc + cur : acc
 // part one answer
 console.log(sum);
 
+const maxDiskSpace = 70_000_000;
 const atLeastSpace = 30_000_000;
-const currentUsedSpace = 70_000_000 - arrayOfValues[0];
+const currentUsedSpace = maxDiskSpace - arrayOfValues[0];
 const minSpaceToDelete = atLeastSpace - currentUsedSpace;
 
 const filteredValues = sortBy(arrayOfValues.filter((val) => val >= minSpaceToDelete));
